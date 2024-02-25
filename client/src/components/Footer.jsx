76081600c582e933e9bb39cefeaf12f63aa0757b
Vignesh <vigneshpamu2002@gmail.com>
2024-02-26 00:14:32 +0530

    sdfsdf

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="h-full bg-slate-800 w-full brder -2 py-10">
-      <footer className="bg-white rounded-lg shadow m-4">
+      <footer className=" rounded-lg shadow m-4">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <a
               href="/"
-              className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+              className="flex items-center mb-4 sm:mb-0 space-x-3 p-5 bg-white rounded-lg rtl:space-x-reverse"
             >
               <img
                 src="/images/logo/logobg.png"
